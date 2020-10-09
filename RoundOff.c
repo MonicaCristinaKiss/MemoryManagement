ac6f064183fef637170550fd82b069898803e65e
Kiss Monica Cristina <71024554+MonicaCristinaKiss@users.noreply.github.com>
2020-10-09 12:08:27 +0300

    Add files via upload
    circumvent errors

---
+++ RoundOff.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	float a=0.25, b=0.3;
+	double c=0.25, d=0.3;
+	printf("a este %.20f\n",a);
+	printf("b este %.20f\n",b);
+	printf("c este %.20lf\n",c);
+	printf("d este %.20lf\n",d);
+	return 0;
+}
