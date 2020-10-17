c2eb959b0531e767f4b56daf2c31c4f1927aee2b
Kiss Monica Cristina <71024554+MonicaCristinaKiss@users.noreply.github.com>
2020-10-17 13:54:40 +0300

    Update ArrayUSize.c

--- ArrayUSize.c
+++ ArrayUSize.c
@@ -3,20 +3,18 @@
 
 int main() {
 	int *a;
-    int n,i = 0;
-
-    a = malloc(sizeof(int));
-
-    do
-    {
-        scanf("%d", &a[i]);
-        a = realloc(a, (i + 2) * sizeof(int));
-    i++;
-    }while (a[i-1] != 0);
+	int n,i = 0;
+	a = malloc(sizeof(int));
+	
+	do {
+        	scanf("%d", &a[i]);
+        	a = realloc(a, (i + 2) * sizeof(int));
+    		i++;
+	}while (a[i-1] != 0);
     
-    n=i;
-    for(i=0;i<n-1;i++) {
-    	printf("%d ",a[i]);
+	n=i;
+    	for(i=0;i<n-1;i++) {
+    		printf("%d ",a[i]);
 	}
 	return 0;
 }
